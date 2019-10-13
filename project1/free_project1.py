d00d862751189a19002636ac7a3cc8be702e0901
Austin Wentworth <asw0019@knights.ucf.edu>
2019-10-13 16:14:11 -0400

    added iteration and conditional

--- project1/free_project1.py
+++ project1/free_project1.py
@@ -2,6 +2,12 @@
 #!/usr/bin/python
 
 # Free Project 7-1: Modify "Stochastic Texts"
+#   I have modified the original program so that it tells of a greeting to one subject
+#   will have some effect on a greeting or farewell of another subject, and the effect 
+#   that it will have on another thing, ending with a final greeting. However, if 'hell'
+#   is mentioned at all, the sentence stops and prints the next sentence. Similarly,
+#   if the word 'damns' is printed for the verbs, it will reduce the counter. When
+#   five "damns" are printed, the function stops.
 # Austin Wentworth
 # DIG 5508
 # Murray
@@ -27,23 +33,36 @@
 #%%
 from random import choice
 
-subjects = ['STRANGER', 'DARKNESS', 'LIGHT', 'CHICKEN', 'FIEND', 'TRAVELER', 'FREAK', 'CRIMINAL', 'GENIUS', 
+subjects = ['HELL', 'STRANGER', 'DARKNESS', 'LIGHT', 'CHICKEN', 'FIEND', 'TRAVELER', 'FREAK', 'CRIMINALS', 'GENIUS', 
               'SLEEP', 'WORLD', 'AMERICA', 'HUMANITY', 'SIR', 'MAAM', 'FANTASY', 'REALITY', 'LOVE', 'HAPPINESS',
               'MISERY', 'AGONY', 'ECSTASY']
 adverbs = ['SLOWLY', 'IMMEDIATELY', 'SURELY', 'INEVITABLY', 'URGENTLY', 'SUDDENLY', 'MESSILY', 'PERFECTLY']
-verbs = ['MEANS', 'CAUSES', 'PREDATES', 'BEGETS', 'ENDS', 'PROLONGS', 'PREVENTS', 'NECESSITATES']
-operators = ['HI', 'HELLO', 'GUTEN TAG', 'HOLA', 'BONJOUR', 'KONICHIWA', 'ALOHA', 'GOODBYE', ]
+verbs = ['MEANS', 'CAUSES', 'PREDATES', 'BEGETS', 'ENDS', 'PROLONGS', 'PREVENTS', 'NECESSITATES', 'DAMNS']
+operators = ['HI', 'GUTEN TAG', 'HOLA', 'BONJOUR', 'KONICHIWA', 'ALOHA', 'GOODBYE', ]
 effect = ['CREATING', 'DESTROYING']
 
-def phrase():
-    text = choice(operators) + ' TO ' + choice(subjects)
+def phrase_iterations():
+    p = 5
+    while(p > 0):
+        text = choice(operators) + ' TO ' + choice(subjects)
+        if 'HELL' in text:
+            text = text + '.'
+        else:
+            text = text + ' ' + choice(verbs) + ' ' + choice(operators) + ' TO ' + choice(subjects)
+            if 'HELL' in text:
+                text = text + '.'
+            else:
+                text = text + ', ' + choice(adverbs) + ' ' + choice(effect) + ' ' + choice(subjects)
+                if 'HELL' in text:
+                    text = text + '.'
+                else:
+                    text = text + '. ' + choice(operators) + '!'
+        print text
+        if 'DAMNS' in text:
+            p = p - 1
     return text
 
-
-print(phrase() + ' ' + choice(verbs) + ' ' + phrase() + ', ' + choice(adverbs) + ' ' + choice(effect)+ ' '+ choice(subjects) + '. ' + choice(operators) + '!')
-
-
-
+phrase_iterations()
 #%%
 
 #%%
