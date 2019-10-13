847db329964f596c4b805e810ce8c605a7cfc0fe
Austin Wentworth <asw0019@knights.ucf.edu>
2019-10-13 16:16:00 -0400

    opened readme

--- project1/free_project1.py
+++ project1/free_project1.py
@@ -2,12 +2,6 @@
 #!/usr/bin/python
 
 # Free Project 7-1: Modify "Stochastic Texts"
-#   I have modified the original program so that it tells of a greeting to one subject
-#   will have some effect on a greeting or farewell of another subject, and the effect 
-#   that it will have on another thing, ending with a final greeting. However, if 'hell'
-#   is mentioned at all, the sentence stops and prints the next sentence. Similarly,
-#   if the word 'damns' is printed for the verbs, it will reduce the counter. When
-#   five "damns" are printed, the function stops.
 # Austin Wentworth
 # DIG 5508
 # Murray
